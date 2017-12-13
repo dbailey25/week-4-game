ad6e09dbf1ffe15a696cee41709e741ef534ba7f
dbailey25 <dtbailey25@gmail.com>
2017-12-12 23:04:27 -0500

    added animation for win

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,8 +9,17 @@ var crystalValues = []
 var crystals = ["blue", "green", "pink", "yellow"]
 
 // functions to be called on page load
+displayShip()
 assignTarget()
 assignCrystalValues()
+
+function displayShip() {
+  $("#launch-pad").html($("<div></div>", {"id": "ship-container"}));
+  $("#ship-container").html($("<div></div", {"id": "ship"}));
+  $("#ship").html($("<img>", {"id": "rocket", "src": "assets/images/ship.png"}));
+  $("#ship-container").append($("<div></div", {"id": "exhaust"}));
+  $("#exhaust").html($("<img>", {"id": "flame", "src": "assets/images/exhaust.png"}));
+}
 
 function assignTarget() {
   targetNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
@@ -38,14 +47,12 @@ function assignCrystalValues() {
        wins++;
        $("#win-count").text(wins);
        $(".crystal").off("click", handler);
+       $("#flame").css("visibility", "visible");
+       var ship = $("#ship-container");
+       ship.animate({top: "-330px"}, 2000);
        $("#instructions").css({"color":"#1add27","border":"4px solid #29511a","background-color":"#4c6d40"});
        $("#instructions").text("Great job! Refueling mission complete.");
-       var flame = $("#flame");
-       flame.animate({height: "30px", width: "25px"});
-       // show replay button
        $("#replay").css("visibility", "visible");
-
-       // !!!!!!!!!!!!!!!! ADD SHIP ANIMATION !!!!!!!!!!!!!!!
      }
 
      else if (counter >= targetNumber) {
@@ -57,7 +64,6 @@ function assignCrystalValues() {
        //  show replay button
        $("#replay").css("visibility", "visible");
 
-       flame.animate({height: "30px", width: "25px"});
 
        // !!!!!!!!!!!!!!!! ADD SHIP ANIMATION !!!!!!!!!!!!!!!
      }
@@ -71,8 +77,10 @@ $(".crystal").on("click", handler)
     $(".crystal").on("click", handler);
     counter = 0;
     $("#current").text("0");
+    displayShip();
     $("#instructions").css({"color":"#000f66","border":"4px solid #000f66","background-color":"#d5d7e0"});
     $("#instructions").text("The ship needs the exact amount of fuel for its mission. Too much and it may explode. Each of the crystals provides a different amount of fuel. Click them to fill its tank to the Target Fuel Level.");
+    $("#flame").css("visibility","hidden");
     assignTarget();
     assignCrystalValues();
     $("#replay").css("visibility", "hidden");
